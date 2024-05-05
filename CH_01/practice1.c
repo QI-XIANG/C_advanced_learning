0ec94b44f39fef3030da6e1d9d0fa6f18ef0a1dd
qixiang <qixiang1009@gmail.com>
2024-05-05 16:40:54 +0800

    add content

---
+++ CH_01/practice1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    printf("我愛C語言");
+
+    //system("pause");
+    return 0;
+}
---
+++ CH_01/practice2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    printf("我愛C語言\n");
+    printf("這是我的第一個C語言程式");
+
+    //system("pause");
+    return 0;
+}
